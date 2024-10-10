96a4850a18a99ecfe02b852bc0d88321b6a1e4c0
LucasClaes <lucas.claes@outlook.com>
2024-10-10 22:00:31 +0200

    Input Handling

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("test")
+import threading
+
+stop_event = threading.Event()
+user_input = None
+
+def input_handler():
+    global user_input  # Globaale variable voor user inputs
+    while not stop_event.is_set():
+        inp = input("Geef iets in: ")
+        if inp.strip().lower() == 'exit':
+            stop_event.set()  # Thread afsluiten bij het stop commando
+        else:
+            user_input = inp
+
+# Keyboard thread starten
+keyboardThread = threading.Thread(target=input_handler)
+keyboardThread.start()
+
+# Simulate using the input elsewhere
+while not stop_event.is_set():
+    if user_input:
+        print(f"Input testen: {user_input}")
+        user_input = None  #Input resetten na gebruik
