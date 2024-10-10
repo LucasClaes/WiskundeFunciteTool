dc19364e3bd386651038cb3ca4158da80700ebbc
LucasClaes <lucas.claes@outlook.com>
2024-10-10 22:42:34 +0200

    Basis Functies

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import input_handling as ih
 import logging as lg
 
 
-
 def Main():
     while not ih.stop_event.is_set():
         if ih.user_input:
             print(f"Input testen: {ih.user_input}")
-            ih.user_input = None  # Reset input after processing
-        ih.threading.Event().wait(0.1)  # Small delay to prevent busy-waiting
+            ih.user_input = None  # User inputs na gebruik resetten
+        ih.threading.Event().wait(0.1)  # Delay om thread niet te overbelasten
 
 if __name__ == '__main__':
-    ih.start_input_thread()  # Start the input handling thread
+    ih.start_input_thread()  # Start de input handling thread
     Main()
-    
