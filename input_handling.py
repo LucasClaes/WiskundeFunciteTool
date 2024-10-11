0e6cb8675ee107037cd1486f99ef5936619b2d58
LucasClaes <lucas.claes@outlook.com>
2024-10-11 08:55:37 +0200

    Basic Functies

--- input_handling.py
+++ input_handling.py
@@ -4,13 +4,13 @@ stop_event = threading.Event()
 user_input = None
 
 def input_handler():
-    global user_input  # Global variable to store user input
+    global user_input  # Globale variable voor user inputs
     while not stop_event.is_set():
         inp = input("Geef iets in: ")
         if inp.strip().lower() == 'exit':
-            stop_event.set()  # Exit command to stop the thread
+            stop_event.set()  # Exit commando om thread af te sluiten
         else:
-            user_input = inp  # Store the input for later use
+            user_input = inp  # Input opslaan voor later gebruik
 
 def start_input_thread():
     keyboardThread = threading.Thread(target=input_handler)
